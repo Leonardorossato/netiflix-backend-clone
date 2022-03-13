42c336e609692bf1eb63e87639ea40c77d41853b
Leonardorossato <leonardorossato21@gmail.com>
2022-03-13 15:10:03 -0300

    metodo controller de login feito

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,21 @@
 const User = require('../models/User')
-const config = require('config')
 const CryptoJs = require('crypto-js')
+
+class UserController{
+    static loginUser = async(req, res) =>{
+        const user = await User.findOne({email: req.body.email})
+        if(user) return res.status(400).json('User already exists. Please try again')
+        const login = new User({
+            username: req.body.username, 
+            email: req.body.email, 
+            password: CryptoJs.AES.encrypt.toString()})
+        try {
+            login.save()
+            return res.status(201).json(login)
+        } catch (error) {
+            return res.status(404).json(error)
+        }
+    }
+}
+
+module.exports = UserController
