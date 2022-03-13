fe80b46de949b5ffe40ef30917ea588b70580010
Leonardorossato <leonardorossato21@gmail.com>
2022-03-13 14:43:11 -0300

    import da conexão com o mongo feita e esta pegando corretamente

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const port = process.env.PORT
+const mongoConnection = require('./connection/mongoConnection')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.mongoConnection = mongoConnection
 
 app.listen(port, ()=>{
     console.log(`Listening on port : ${port}`)
