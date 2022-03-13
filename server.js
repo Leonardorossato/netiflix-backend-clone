c34557d4117f5ca60d04f120d5cf5ad70736f873
Leonardorossato <leonardorossato21@gmail.com>
2022-03-13 15:09:51 -0300

    import da rota user

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 const port = process.env.PORT
 const mongoConnection = require('./connection/mongoConnection')
+const userRouter = require('./router/userRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.mongoConnection = mongoConnection
+
+app.use('/api/user', userRouter)
 
 app.listen(port, ()=>{
     console.log(`Listening on port : ${port}`)
