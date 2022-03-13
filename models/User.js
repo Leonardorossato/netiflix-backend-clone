795acc977714d8aaab5f6f48c5fba2b5d32913bc
Leonardorossato <leonardorossato21@gmail.com>
2022-03-13 15:10:24 -0300

    ajsute

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     userName : { type : String, required: true },
-    email : { type : String, required: true, unique},
+    email : { type : String, required: true, unique: true },
     password : { type : String, required: true},
     profilePicture : { type : String, default: ''},
     isAdmin : {type : Boolean, default: false}
